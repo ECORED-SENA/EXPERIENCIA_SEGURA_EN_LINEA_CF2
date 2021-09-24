d5c15bf92122a066e6ad4abbb92532eaaa8a683d
Mao <mao8a87@gmail.com>
2021-09-24 11:48:29 -0500

    primer commit

---
+++ src/views/Introduccion.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.curso-main-container.introduccion
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+
+</template>
+
+<script>
+export default {
+  name: 'Introduccion',
+  data: () => ({
+    // variables de vue
+  }),
+}
+</script>
+
+<style lang="sass" scoped>
+.introduccion
+</style>
